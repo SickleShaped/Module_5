eb42f1dfa50a9a42ec56f4b776ff1b6bf843f98b
Филиппов Алексей <teenwizard.fornewvideo@gmail.com>
2024-03-19 16:34:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Module_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Dishes = new string[5];
+            
+            (string Name, string[] Dishes) User = ("Санни", Dishes);
+            
+            for(int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"Введите любимое блюда пользователя {User.Name} номер {i+1}");
+                User.Dishes[i] = Console.ReadLine();
+            }
+
+        }
+    }
+}

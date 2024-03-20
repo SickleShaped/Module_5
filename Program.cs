07b8c0c7187ae66b4fc2d14c37d0a2562a2a0a86
Филиппов Алексей <teenwizard.fornewvideo@gmail.com>
2024-03-20 18:55:21 +0300

    colored console

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@
             {
                 modif = modif.Remove(0, 2);
             }
+            Console.BackgroundColor = (ConsoleColor)deep;
             Console.WriteLine("..."+modif);
 
             if (deep > 1)
